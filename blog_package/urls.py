06b38c1fb856fc0241f3094a24e56447efde2896
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-24 15:27:29 -0400

    staticfiles configured

--- blog_package/urls.py
+++ blog_package/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import blog
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('blog.urls',
-                      namespace='blog')),
+    url(r'^admin/',
+        include(admin.site.urls)),
+    url(r'^blog/',
+        include('blog.urls',
+                namespace='blog')),
+    url(r'^', include('portfolio.urls',
+                      namespace='portfolio')),
 ]
