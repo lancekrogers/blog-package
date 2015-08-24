a2e9e509433016e4bf9803546779808aa497a644
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-24 19:05:59 -0400

    staticfiles are annoying

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
-
+from .views import portfolio
 
 urlpatterns = [
+    url(r'^', portfolio,  name='home'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def portfolio(request):
+    return render(request, 'portfolio/index.html')
+   # return HttpResponse('Hello')
