120d044036b50706b6d90e4873536014fe75f3c0
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-24 19:53:21 -0400

    staticfiles configured

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from .views import portfolio
+from .views import portfolio, home
 
 urlpatterns = [
     url(r'^', portfolio,  name='home'),
+    url(r'^home/$', home)
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def portfolio(request):
-    return render(request, 'portfolio/index.html')
-   # return HttpResponse('Hello')
+    return render(request, 'portfolio/portfolio.html')
+
+
+def home(request):
+    return render(request, 'portfolio/home.html')
