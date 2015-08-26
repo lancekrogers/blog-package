9ba7d38975b81008c004fc4e449049a7cdf1870b
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-26 14:06:33 -0400

    static root changed

--- blog_package/settings.py
+++ blog_package/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xh(z&$e84kv*635&ct-ct(jogyyv@jpkuxljgb+dg2(x^z4o+m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -103,13 +103,13 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 #STATICFILES_DIRS = (
  #   os.path.join(BASE_DIR, 'static/'),
 #)
 
-STATIC_ROOT = '/static'
+#STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.sep.join([os.path.dirname(os.path.dirname(__file__)), 'static']),
