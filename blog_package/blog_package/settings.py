42ed784dac67534ac8f014163a9f60bdbd31d84d
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-26 22:07:27 -0400

    psql added

--- blog_package/blog_package/settings.py
+++ blog_package/blog_package/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'blog_package.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',  # 'db_name',
+        'USER': 'blog',  # 'db_user',
     }
 }
 
