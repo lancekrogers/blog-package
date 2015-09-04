e0346403fc786ac8302530e8139f7c6251d8cce7
Lance Rogers <lancekincaid1994@gmail.com>
2015-09-03 22:07:14 -0400

    first migration

--- blog_package/blog/models.py
+++ blog_package/blog/models.py
@@ -5,14 +5,21 @@ from PIL import Image  # this is needed for the models.ImageField to work
 # Create your models here.
 
 
-
 class Post(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=60, blank=False)
-    body = models.TextField(blank=True, null=True)
+    body = models.ManyToManyField('Paragraph')
+    date = models.DateField(auto_now_add=True)
+    photos = models.ManyToManyField('Photo')
 
 
 class Photo(models.Model):
-    title = models.CharField(max_length=30, blank=True)
-    photo = models.ImageField()
+    blog_post = models.ForeignKey(Post, blank=True, null=True)
+    title = models.CharField(max_length=30, blank=True, null=True)
+    photo = models.ImageField(upload_to='profile_image/%Y/%m/%d', blank=True)
+    date = models.DateField(auto_now_add=True)
 
+
+class Paragraph(models.Model):
+    blog_post = models.ForeignKey(Post)
+    paragraph = models.TextField(blank=True, null=True)
--- blog_package/blog_package/settings.py
+++ blog_package/blog_package/settings.py
@@ -104,15 +104,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
-#STATICFILES_DIRS = (
- #   os.path.join(BASE_DIR, 'static/'),
-#)
-
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.sep.join([os.path.dirname(os.path.dirname(__file__)), 'static']),
 )
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR,
+                          "static/media")
+MEDIA_URL = '/media/'
